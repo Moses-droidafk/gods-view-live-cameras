68ae7ffa58e2177a7ae9cfa81c677ae6187c27b9
Moses-droidafka <mosesxtiandeluka@gmail.com>
2026-06-26 23:45:42 +0300

    Connected app to TiDB cloud database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require("express");
+require('dotenv').config();const express = require("express");
+
 const mysql = require("mysql2");
 const session = require("express-session");
 const bcrypt = require("bcrypt");
@@ -26,10 +27,15 @@ app.set("view engine", "ejs");
 // ======================
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "mosesdeluka001",
-    database: "camera_system"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true
+    }
 });
 
 db.connect((err) => {
